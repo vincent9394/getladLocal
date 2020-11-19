c9866609786e54001ffada1962e2d02dd3719e11
Harriet <hshin2007@gmail.com>
2020-11-19 20:56:06 +0800

    2020/11/19 added foodie-group, 80%complete navbar,70%completed footer

--- server.ts
+++ server.ts
@@ -7,6 +7,9 @@ app.use(express.static('public'))
 app.get('/entertainment', async(req, res)=> {
     res.redirect('./entertainment.html')
 })
+app.get('/foodie-group', async(req, res)=> {
+    res.redirect('./foodie-group.html')
+})
 
 let port = 8080
 app.listen(port, () => {
