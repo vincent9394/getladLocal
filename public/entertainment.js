89882c5d0d8922f25c9533222fb68571af013ecb
Harriet <hshin2007@gmail.com>
2020-11-19 16:41:34 +0800

    entertainment: added map and card

---
+++ public/entertainment.js
@@ -0,0 +1,61 @@
+///////////////// fixed menu on scroll for desktop
+if ($(window).width() > 992) {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 40) {
+            $('#navbar_top').addClass("fixed-top");
+            // add padding top to show content behind navbar
+            $('body').css('padding-top', $('.navbar').outerHeight() + 'px');
+        } else {
+            $('#navbar_top').removeClass("fixed-top");
+            // remove padding top from body
+            $('body').css('padding-top', '0');
+        }
+    });
+} // end if
+
+let map;
+let infoWindow;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 5,
+    });
+    
+}
+
+
+
+let gridtoggle = false
+let gridOption = document.querySelector('.gridOption')
+let gridChoiceFlex = document.querySelector('.gridChoiceFlex')
+let ans = gridOption.addEventListener('click', function () {
+    gridtoggle = !gridtoggle
+    if (gridtoggle == false) {
+        gridChoiceFlex.style.display = "none"
+    } else if (gridtoggle == true) {
+        gridChoiceFlex.style.display = "flex"
+    }
+})
+
+let gridChoices = document.querySelectorAll('.gridChoice')
+for (let gridChoice of gridChoices) {
+    gridChoice.addEventListener("click", function () {
+        gridChoiceFlex.style.display = "none"
+        toggle = false
+    })
+}
+
+let buttonToggle = false
+let bookmarkButtons = document.querySelectorAll('.fa-bookmark')
+for (let bookmarkButton of bookmarkButtons) {
+    bookmarkButton.addEventListener('click', function () {
+        buttonToggle = !buttonToggle
+        if (buttonToggle == false) {
+            bookmarkButton.style.color = "#D8D6D9"
+        } else if (buttonToggle == true) {
+            bookmarkButton.style.color = "#F3C20C"
+        }
+    }
+    )
+}
