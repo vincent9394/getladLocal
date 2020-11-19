1bc9e4feed16012a07ac4da3545b7d0ddc95be8e
michaellamcs <michaellamcs@gmail.com>
2020-11-19 14:04:35 +0800

    refining

--- public/index.js
+++ public/index.js
@@ -1,4 +1,20 @@
+let toggle = false
 let gridOption = document.querySelector('.gridOption')
-gridOption.addEventListener('click', function() {
-    
+let gridChoiceFlex = document.querySelector('.gridChoiceFlex')
+let ans = gridOption.addEventListener('click', function() {
+    if(toggle == false) {
+        gridChoiceFlex.style.display = "flex"
+        toggle = true
+    } else if (toggle == true) {
+        gridChoiceFlex.style.display = "none"
+        toggle = false
+    }
 })
+
+let gridChoices = document.querySelectorAll('.gridChoice')
+for (let gridChoice of gridChoices) {
+    gridChoice.addEventListener("click", function (){
+        gridChoiceFlex.style.display = "none"
+        toggle = false
+    })
+}
