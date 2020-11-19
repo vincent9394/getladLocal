6b62c70ff1f26ba6a810a32d7acb3a54e71aec06
michaellamcs <michaellamcs@gmail.com>
2020-11-19 15:54:06 +0800

    card

--- public/index.js
+++ public/index.js
@@ -1,20 +1,33 @@
-let toggle = false
+let gridtoggle = false
 let gridOption = document.querySelector('.gridOption')
 let gridChoiceFlex = document.querySelector('.gridChoiceFlex')
-let ans = gridOption.addEventListener('click', function() {
-    if(toggle == false) {
-        gridChoiceFlex.style.display = "flex"
-        toggle = true
-    } else if (toggle == true) {
+let ans = gridOption.addEventListener('click', function () {
+    gridtoggle = !gridtoggle
+    if (gridtoggle == false) {
         gridChoiceFlex.style.display = "none"
-        toggle = false
+    } else if (gridtoggle == true) {
+        gridChoiceFlex.style.display = "flex"
     }
 })
 
 let gridChoices = document.querySelectorAll('.gridChoice')
 for (let gridChoice of gridChoices) {
-    gridChoice.addEventListener("click", function (){
+    gridChoice.addEventListener("click", function () {
         gridChoiceFlex.style.display = "none"
         toggle = false
     })
+}
+
+let buttonToggle = false
+let bookmarkButtons = document.querySelectorAll('.fa-bookmark')
+for (let bookmarkButton of bookmarkButtons) {
+    bookmarkButton.addEventListener('click', function () {
+        buttonToggle = !buttonToggle
+        if (buttonToggle == false) {
+            bookmarkButton.style.color = "#D8D6D9"
+        } else if (buttonToggle == true) {
+            bookmarkButton.style.color = "#F3C20C"
+        }
+    }
+    )
 }
